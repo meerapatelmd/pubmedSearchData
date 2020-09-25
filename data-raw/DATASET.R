1055071b035bfa92073ae13e11525ac315e35179
Meera <patelmeeray@gmail.com>
2020-09-25 11:15:38 -0400

    change DATASET.R broca to readr

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
-library(broca)
+library(readr)
 
-RESULTS <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/pubmedSearchData//data-raw//RESULTS.csv')
-RESULTS_LOG <- broca::simply_read_csv('/Users/patelm9/GitHub/Public-Packages/pubmedSearchData//data-raw//RESULTS_LOG.csv')
+RESULTS <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/pubmedSearchData//data-raw//RESULTS.csv')
+RESULTS_LOG <- readr::read_csv('/Users/patelm9/GitHub/Public-Packages/pubmedSearchData//data-raw//RESULTS_LOG.csv')
 
 usethis::use_data(RESULTS,RESULTS_LOG, overwrite = TRUE)
